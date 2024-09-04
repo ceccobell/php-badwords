5a5ab79cc6f66a206289be4a7c0495d11b56ce0c
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-09-04 14:49:32 +0200

    [UPD] exercise completed

--- welcome.php
+++ welcome.php
@@ -15,6 +15,11 @@
         echo "<p>$paragrafo</p>";
         $lunghezza = strlen($paragrafo);
         echo "<p>Lunghezza: $lunghezza caratteri</p>";
+
+        $paragrafoCensurato = str_replace($parola, '***', $paragrafo);
+
+        echo "<h2>paragrafo censurato:</h2>";
+        echo "<p>$paragrafoCensurato</p>";
     ?>
 </body>
 </html>
