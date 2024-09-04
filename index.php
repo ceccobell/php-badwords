c89ebef04b4af371c42512dffc63f80992a67e77
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-09-04 14:40:51 +0200

    [UPD] linked two files and added function to print paragrafo

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form di Censura</title>
+    <title>Form da Censurare</title>
 </head>
 <body>
     <h2>Inserisci il paragrafo e la parola da censurare</h2>
-    <form action="" method="GET">
+    <form action="welcome.php" method="GET">
         <label for="paragrafo">Paragrafo:</label><br>
         <textarea name="paragrafo" id="paragrafo" rows="5" cols="40" required></textarea><br><br>
 
--- welcome.php
+++ welcome.php
@@ -1,4 +1,18 @@
 <?php
-    $paragrafo = $_GET['paragrafo']
-    $parola = $_GET['parola']
+    $paragrafo = $_GET['paragrafo'];
+    $parola = $_GET['parola'];
 ?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Censurato</title>
+</head>
+<body>
+    <?php
+        echo $paragrafo
+    ?>
+</body>
+</html>
